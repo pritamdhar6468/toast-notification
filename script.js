765f8e6f1987664b1ad184c1804744ccba4eb154
Pritamdhar_official <pritamdhar6568@gmail.com>
2022-12-27 10:13:18 +0530

    build a complete responsive toast notification

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const notification = document.querySelector('.notification');
 const buttons = document.querySelectorAll('.buttons .btn');
 
 const toastDetails ={
+    timer:5000,
     success:{
         icon:'fa-circle-check',
         text:'Success: This is a success toast',
@@ -20,6 +21,12 @@ const toastDetails ={
     },
 }
 
+const removeToaste = (toast)=> {
+    toast.classList.add("hide");
+    if(toast.timeoutId) clearTimeout(toast.timeoutId); //clearing the timeout for the toast
+    setTimeout(()=> toast.remove(),500);  //removing the toast after 500ms
+}
+
 const createToast = (id) => {
     //getting the icon and text for the toast based on the id passed
     const { icon,text } = toastDetails[id];
@@ -30,9 +37,11 @@ const createToast = (id) => {
                             <i class="fa-solid ${icon}"></i>
                             <span>${text}</span>
                         </div>
-                        <i class="fa-solid fa-xmark"></i>`;
+                        <i class="fa-solid fa-xmark" onclick="removeToaste(this.parentElement)"></i>`;
 
-    notification.appendChild(toast); //append the toast to the notification ul                    
+    notification.appendChild(toast); //append the toast to the notification ul      
+    //setting the timeout to remove the toaste after the specified duration
+    toast.timeoutId = setTimeout(() => removeToaste(toast),toastDetails.timer);              
 }
 
 buttons.forEach(btn => {
